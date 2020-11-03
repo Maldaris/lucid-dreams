06def9a029b44b57c5147f5ea42400531656989e
Shayne Fitzgerald <robotech417@yahoo.com>
2020-11-03 13:27:48 -0500

    complete header parser

--- src/disassembly/dmb/header.rs
+++ src/disassembly/dmb/header.rs
@@ -1,11 +1,16 @@
-use super::Context;
+extern crate easy_strings;
 
-const DEFAULT_VERSION : &'static i8 = &(230 as i8);
+use easy_strings::{EZString, ez};
+
+use super::Context;
+use super::Readable;
+
+const DEFAULT_VERSION : &'static u16 = &(230 as u16);
 
 pub struct Header {
-   v_gen: i8,
-   v_lhs: i8,
-   v_rhs: i8,
+   v_gen: u16,
+   v_lhs: u16,
+   v_rhs: u16,
    flags: i32,
    ex_flags: i32,
 }
@@ -21,9 +26,63 @@ impl Header {
        }
     }
 
-    pub fn read(&mut self, mut ctx: &Context) {
-        loop {
+    pub fn read(&mut self, ctx: &mut Context) {
+        let mut b : u8 = 0;
+        let mut s = ez("");
+        let mut shebang = ez("");
+        let mut compat_line : bool = false;
 
+        loop {
+            (*ctx).u8(&mut b);
+            if b == 10 {
+                if !compat_line {
+                    if s.starts_with("#") {
+                        shebang += &s + &ez("\n");
+                        (ctx.base_pointer) = ctx.position as u32;
+                    } else {
+                        let split = s.split(" v").collect::<Vec<EZString>>();
+                        let tmp = split[1].parse::<u16>().unwrap();
+                        self.v_rhs = tmp.clone();
+                        self.v_lhs = tmp.clone();
+                        self.v_gen = tmp.clone();
+                        compat_line = true;
+                    }
+                } else {
+                    let version_bits = s.split(" v")
+                        .collect::<Vec<EZString>>()
+                        [1].split(" ")
+                        .collect::<Vec<EZString>>();
+                    if version_bits.len() < 2 {
+                        self.v_lhs = version_bits[0].parse::<u16>().unwrap();
+                        self.v_rhs = self.v_lhs.clone();
+                    } else {
+                        self.v_lhs = version_bits[0].parse::<u16>().unwrap();
+                        self.v_rhs = version_bits[1].parse::<u16>().unwrap();
+                    }
+
+                    break;
+                }
+                s = ez("");
+            } else {
+                s.push(b as char);
+            }
+        }
+
+        ctx.u32(&mut (self.flags as u32));
+        self.ex_flags = 0;
+        if (self.flags as u32 & 0x80000000) != 0 {
+            ctx.u32(&mut (self.ex_flags as u32));
+        }
+
+        ctx.v_gen = self.v_gen.clone();
+        ctx.v_lhs = self.v_lhs.clone();
+        ctx.v_rhs = self.v_rhs.clone();
+        ctx.large_object_ids = (self.flags as u32 & 0x40000000) != 0;
+
+        if ctx.debug {
+            println!("version: {} {} {}", self.v_gen, self.v_lhs, self.v_rhs);
+            println!("large object ids: {}", ctx.large_object_ids);
+            println!("base pointer: {}", ctx.base_pointer);
         }
     }
 }
--- src/disassembly/dmb/mod.rs
+++ src/disassembly/dmb/mod.rs
@@ -26,35 +26,39 @@ impl<'a> DMB {
         }
     }
 
-    pub fn read(&mut self, ctx: Context<'a>) {
-        self.header.read(&ctx);
+    pub fn read(&mut self, ctx: &mut Context<'a>) {
+        self.header.read(ctx);
     }
 }
 
 trait Readable {
-    fn i8(&mut self, i: &mut i8);
-    fn i16(&mut self, i: &mut i16);
-    fn i32(&mut self, i: &mut i32);
+    fn u8(&mut self, i: &mut u8);
+    fn u16(&mut self, i: &mut u16);
+    fn u32(&mut self, i: &mut u32);
     fn f32(&mut self, f : &mut f64);
-    fn ints(&mut self, group : &mut [i32]);
+    fn ints(&mut self, group : &mut [u32]);
     fn floats(&mut self, group : &mut [f64]);
-    fn bytes(&mut self, group : &mut [i8]);
+    fn bytes(&mut self, group : &mut [u8]);
 }
 
 pub struct Context<'a> {
     debug : bool,
-    data : &'a[i8],
+    data : &'a[u8],
     position: usize,
-    largeObjectIds: bool,
+    base_pointer: u32,
+    large_object_ids: bool,
+    v_gen: u16, v_lhs: u16, v_rhs: u16
 }
 
 impl<'a> Context<'a> {
-    pub fn new(bytes: &'a[i8]) -> Context {
+    pub fn new(bytes: &'a[u8]) -> Context {
         Context {
             debug: DEBUG_ENABLED,
             position: 0,
+            base_pointer: 0,
             data: bytes,
-            largeObjectIds: false
+            large_object_ids: false,
+            v_gen: 0, v_lhs: 0, v_rhs: 0
         }
     }
 }
@@ -66,42 +70,42 @@ impl Context<'_> {
             println!("{} + @ 0x {:X}", text, self.position)
         }
     }
-    fn id(&mut self, container: &mut i32) {
-        if self.largeObjectIds {
-            return self.i32(container);
+    fn id(&mut self, container: &mut u32) {
+        if self.large_object_ids {
+            return self.u32(container);
         }
-        self.i16(&mut (*container as i16));
+        self.u16(&mut (*container as u16));
         (*container) = (*container) & 0xFFFF;
     }
 }
 
 impl<'a> Readable for Context<'a> {
-    fn i8(&mut self, i: &mut i8) {
+    fn u8(&mut self, i: &mut u8) {
         (*i) = self.data[self.position];
         self.position += 1;
     }
-    fn i16(&mut self, i: &mut i16) {
-        let mut byte : i8 = 0;
-        self.i8(&mut byte);
-        let lower = (byte & 0xF) as i16;
-        self.i8(&mut byte);
-        (*i) = ((byte & 0xF) as i16) << 8 | lower;
+    fn u16(&mut self, i: &mut u16) {
+        let mut byte : u8 = 0;
+        self.u8(&mut byte);
+        let lower = (byte & 0xFF) as u16;
+        self.u8(&mut byte);
+        (*i) = ((byte & 0xFF) as u16) << 8 | lower;
     }
-    fn i32(&mut self, i: &mut i32) {
-        let mut short : i16 = 0;
-        self.i16(&mut short);
-        let lower = (short & 0xFF) as i32;
-        self.i16(&mut short);
-        (*i) = (((short & 0xFF) as i32) << 16) | lower;
+    fn u32(&mut self, i: &mut u32) {
+        let mut short : u16 = 0;
+        self.u16(&mut short);
+        let lower = (short & 0xFFFF) as u32;
+        self.u16(&mut short);
+        (*i) = (((short & 0xFFFF) as u32) << 16) | lower;
     }
     fn f32(&mut self, f: &mut f64) {
-        let mut int: i32 = 0;
-        self.i32(&mut int);
+        let mut int: u32 = 0;
+        self.u32(&mut int);
         (*f) = int as f64;
     }
-    fn ints(&mut self, group : &mut [i32]) {
+    fn ints(&mut self, group : &mut [u32]) {
         for i in 0..group.len() {
-            self.i32(&mut group[i])
+            self.u32(&mut group[i])
         }
     }
     fn floats(&mut self, group : &mut [f64]) {
@@ -109,9 +113,9 @@ impl<'a> Readable for Context<'a> {
             self.f32(&mut group[i])
         }
     }
-    fn bytes(&mut self, group : &mut [i8]) {
+    fn bytes(&mut self, group : &mut [u8]) {
         for i in 0..group.len() {
-            self.i8(&mut group[i])
+            self.u8(&mut group[i])
         }
     }
 }
